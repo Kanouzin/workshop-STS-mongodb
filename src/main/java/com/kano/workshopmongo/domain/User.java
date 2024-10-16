8301de77e66733a1fd41d64914616025e9bd9b0c
Kanouzin <allansantos296@gmail.com>
2024-10-16 20:00:46 -0300

    Busca de usuario por Id

--- src/main/java/com/kano/workshopmongo/domain/User.java
+++ src/main/java/com/kano/workshopmongo/domain/User.java
@@ -8,73 +8,61 @@ import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document(collection="User")
 public class User implements Serializable {
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
-	@Id
-	private String Id;
-	private String name;
-	private String email;
+    @Id
+    private String id;
+    private String name;
+    private String email;
 
-	public User() {
-	}
+    public User() {
+    }
 
-	public User(String id, String name, String email) {
-		super();
-		Id = id;
-		this.name = name;
-		this.email = email;
-	}
+    public User(String id, String name, String email) {
+        super();
+        this.id = id;
+        this.name = name;
+        this.email = email;
+    }
 
-	public String getId() {
-		return Id;
-	}
+    public String getId() {
+        return id;
+    }
 
+    public void setId(String id) {
+        this.id = id;
+    }
 
+    public String getName() {
+        return name;
+    }
 
-	public void setId(String id) {
-		Id = id;
-	}
+    public void setName(String name) {
+        this.name = name;
+    }
 
+    public String getEmail() {
+        return email;
+    }
 
+    public void setEmail(String email) {
+        this.email = email;
+    }
 
-	public String getName() {
-		return name;
-	}
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 
-
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-
-
-	public String getEmail() {
-		return email;
-	}
-
-
-
-	public void setEmail(String email) {
-		this.email = email;
-	}
-
-
-
-	@Override
-	public int hashCode() {
-		return Objects.hash(Id);
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		User other = (User) obj;
-		return Objects.equals(Id, other.Id);
-	}
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        User other = (User) obj;
+        return Objects.equals(id, other.id);
+    }
 }
--- src/main/java/com/kano/workshopmongo/services/UserService.java
+++ src/main/java/com/kano/workshopmongo/services/UserService.java
@@ -1,19 +1,26 @@
 package com.kano.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.kano.workshopmongo.domain.User;
 import com.kano.workshopmongo.repository.UserRepository;
+import com.kano.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
 	@Autowired
 	private UserRepository repo;
-	
+
 	public List<User> findAll() {
 		return repo.findAll();
+	}
+
+	public User findById(String id) {
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 }
